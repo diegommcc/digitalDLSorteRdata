7d69eed3d574b5b1a0e6282ee9901f24e187b8c8
Diego Mañanes Cayero <CNIC\dmananesc@cnic-29308.cnic.es>
2021-08-09 11:36:19 +0200

    Base implementation

---
+++ R/datasetsExamples.R
@@ -0,0 +1,22 @@
+#' Breast cancer bulk RNA-Seq samples from TCGA Research Network
+#'
+#' Subset of breast cancer bulk RNA-Seq samples from TCGA Research Network.
+#' FPKMs were transformed to TPMs and aggregated based on SYMBOL genes.
+#'
+#' @source \url{https://www.cancer.gov/tcga}
+#'
+#' @references Koboldt, D. C., Fulton, R. S., McLellan, M. D., Schmidt, H.,
+#'   Kalicki-Veizer, J., McMichael, J. F., et al. (2012). Comprehensive
+#'   molecular portraits of human breast tumours. Nature 490 (7418), 61–70. doi:
+#'   10.1038/nature11412
+#'
+#'   Ciriello, G., Gatza, M. L., Beck, A. H., Wilkerson, M. D., Rhie, S. K.,
+#'   Pastore, A., et al. (2015). Comprehensive molecular portraits of invasive
+#'   lobular breast cancer. Cell. 163 (2), 506–519. doi:
+#'   10.1016/j.cell.2015.09.033
+#'
+#'   Torroja, C. y Sánchez-Cabo, F. (2019). digitalDLSorter: A Deep Learning
+#'   algorithm to quantify immune cell populations based on scRNA-Seq data.
+#'   Frontiers in Genetics 10, 978. doi: \url{10.3389/fgene.2019.00978}
+#'
+"TCGA.breast.small"
---
+++ R/ddlsObjExamples.R
@@ -0,0 +1,76 @@
+#' \code{\link{DigitalDLSorter}} object with Chung et al. 2017 data set
+#'
+#' \code{\link{DigitalDLSorter}} object containing the
+#' original data set used to generate \code{breast.chung.generic} and
+#' \code{breast.chung.specific} models in order to show some examples in
+#' vignette and documentation. It contains a reduced version of the original
+#' \code{ZinbParams} object in \code{zinb.params} slot. Data is stored as a
+#' \code{SingleCellExperiment} object with counts in \code{assay} slot, cells
+#' metadata in \code{colData} slot and genes metadata in \code{rowData} slot.
+#'
+#' For more information about the data set and the models, see
+#' \code{breast.chung.generic} or \code{breast.chung.specific}.
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75688}
+#'
+#' @references Chung, W., Eum, H. H., Lee, H. O., Lee, K. M., Lee, H. B., Kim,
+#'   K. T., et al. (2017). Single-cell RNA-seq enables comprehensive tumour and
+#'   immune cell profiling in primary breast cancer. Nat. Commun. 8 (1), 15081.
+#'   doi: \url{10.1038/ncomms15081}.
+#'
+#'   Torroja, C. y Sánchez-Cabo, F. (2019). digitalDLSorter: A Deep Learning
+#'   algorithm to quantify immune cell populations based on scRNA-Seq data.
+#'   Frontiers in Genetics 10, 978. doi: \url{10.3389/fgene.2019.00978}
+#'
+"DDLSChung"
+
+#' \code{\link{DigitalDLSorter}} object with Li et al. 2017 data set
+#'
+#' \code{\link{DigitalDLSorter}} object containing single-cell RNA-seq data from
+#' Li at al., 2017 consisted of 11 samples from patients with colorectal cancer
+#' in different stages and grades and different gender. It contains a reduced
+#' version of the original \code{ZinbParams} object in \code{zinb.params} slot.
+#' Data is stored as a \code{SingleCellExperiment} object with counts in
+#' \code{assay} slot, cells metadata in \code{colData} slot and genes metadata
+#' in \code{rowData} slot.
+#'
+#' For more information about the data set, please see Torroja and Sanchez-Cabo,
+#' 2019 and Li et al., 2017
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75688}
+#'
+#' @references Li, H., Courtois, E. T., Sengupta, D., Tan, Y., Chen, K. H., Goh,
+#'   J. J. L., et al. (2017). Reference component analysis of single-cell
+#'   transcriptomes elucidates cellular heterogeneity in human colorectal
+#'   tumors. Nat. Genet. 49 (5), 708–718. doi: \url{10.1038/ng.3818}
+#'
+#'   Torroja, C. y Sánchez-Cabo, F. (2019). digitalDLSorter: A Deep Learning
+#'   algorithm to quantify immune cell populations based on scRNA-Seq data.
+#'   Frontiers in Genetics 10, 978. doi: \url{10.3389/fgene.2019.00978}
+#'   
+"DDLSLi"
+
+#' \code{\link{DigitalDLSorter}} object with Li et al. 2017 data set (trained
+#' version)
+#'
+#' \code{\link{DigitalDLSorter}} object containing a trained model using
+#' single-cell RNA-seq data from Li at al., 2017. It consists of 11 samples fro`m
+#' patients with colorectal cancer in different stages and grades, and different
+#' gender. It contains only the DDLS model in order to provide realistic results
+#' in vignettes.
+#'
+#' For more information about the data set, please see Torroja and Sanchez-Cabo,
+#' 2019 and Li et al., 2017
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75688}
+#'
+#' @references Li, H., Courtois, E. T., Sengupta, D., Tan, Y., Chen, K. H., Goh,
+#'   J. J. L., et al. (2017). Reference component analysis of single-cell
+#'   transcriptomes elucidates cellular heterogeneity in human colorectal
+#'   tumors. Nat. Genet. 49 (5), 708–718. doi: \url{10.1038/ng.3818}
+#'
+#'   Torroja, C. y Sánchez-Cabo, F. (2019). digitalDLSorter: A Deep Learning
+#'   algorithm to quantify immune cell populations based on scRNA-Seq data.
+#'   Frontiers in Genetics 10, 978. doi: \url{10.3389/fgene.2019.00978}
+#'   
+"DDLSLiComp"
