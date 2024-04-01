d078b80d22d12daa032480370d811375b28c6f32
diegommcc <diegommcc96@gmail.com>
2024-04-01 15:49:14 +0200

    DDLS object removed. Loaded scRNA-seq data with colorectal cancr instead

--- R/datasetsExamples.R
+++ R/datasetsExamples.R
@@ -43,3 +43,28 @@
 #'   Frontiers in Genetics 10, 978. doi: \doi{10.3389/fgene.2019.00978}
 #'
 "TCGA.colon.se"
+
+
+#' Colorectal cancer single-cell RNA-seq dataset from Lee, Hong, 
+#' Etlioglu Cho et al., 2020  
+#'
+#' \code{SingleCellExperiment} object containing single-cell RNA-seq data from 
+#' colorectal cancer samples from the Lee, Hong, Etlioglu Cho et al., 2020 
+#' study (GSE132465, GSE132257 and GSE144735).The raw count matrix only contains
+#' information for the 2,000 genes selected to build the \code{DDLS.colon.lee} 
+#' pre-trained model from the \code{digitalDLSorteRmodels} R package. 
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE75688}
+#'   \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE132257}
+#'   \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE144735}
+#'
+#' @references Lee HO, Hong Y, Etlioglu HE, Cho YB, Pomella V, Van den Bosch B, 
+#'   Vanhecke J, Verbandt S, Hong H, Min JW, Kim N, Eum HH, Qian J, Boeckx B, 
+#'   Lambrechts D, Tsantoulis P, De Hertogh G, Chung W, Lee T, An M, Shin HT, 
+#'   Joung JG, Jung MH, Ko G, Wirapati P, Kim SH, Kim HC, Yun SH, Tan IBH, 
+#'   Ranjan B, Lee WY, Kim TY, Choi JK, Kim YJ, Prabhakar S, Tejpar S, Park WY. 
+#'   Lineage-dependent gene expression programs influence the immune landscape 
+#'   of colorectal cancer. Nat Genet. 2020 Jun;52(6):594-603. 
+#'   doi: \doi{10.1038/s41588-020-0636-z}.
+#'
+"SCE.colon.Lee"
